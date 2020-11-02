1b85f50cb870171be7b52d0193d39b327eab383a
Anidhya Gupta <guptaanidhya2000.fb@gmail.com>
2020-11-02 16:51:41 +0530

    Problem uploaded

--- Nearly-Lucky-Number/main.cpp
+++ Nearly-Lucky-Number/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(){
-    
+    string number;
+    cin >> number;
+    int n{0};
+    for(size_t i{0};i<number.length();i++){
+        if(number[i]=='4' || number[i]=='7')
+            n++;
+    }
+    if(n==4 || n==7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
     return 0;
 }
